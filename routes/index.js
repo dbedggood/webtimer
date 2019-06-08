6e0f4d66ef5fbca6eb04cb087ee0b2b66621d8d5
Daniel Bedggood <ddbedggood@gmail.com>
2019-06-08 15:45:54 +1200

    Display url path on page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get(/^\/(\d+h)?(\d+m)?$/, function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: "Express.js" , message: req.path});
 });
 
 module.exports = router;
