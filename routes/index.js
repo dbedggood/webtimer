72c5002e27e098078187fb3d370c39ec4e0f05c7
Daniel Bedggood <ddbedggood@gmail.com>
2019-06-08 18:52:58 +1200

    Add page if no timer is set, add instructions

--- routes/index.js
+++ routes/index.js
@@ -1,62 +1,65 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 /* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { message: 'No timer set.' })
+})
+
 router.get(/^\/(\d+h)?(\d+m)?$/, function(req, res, next) {
-  const time  = req.path.slice(1)
-  let hours = ""
-  let minutes = ""
-  let output = ""
-  const hIndex = time.indexOf("h")
-  const mIndex = time.indexOf("m")
+    const time = req.path.slice(1)
+    let hours = ''
+    let minutes = ''
+    let output = ''
+    const hIndex = time.indexOf('h')
+    const mIndex = time.indexOf('m')
 
-  if (hIndex < 0) {
-    hours = "0"
-    minutes = time.slice(0, mIndex)
-  } else if (mIndex < 0) {
-    hours = time.slice(0, hIndex)
-    minutes = "0"
-  } else {
-    hours = time.slice(0, hIndex)
-    minutes = time.slice(hIndex + 1, mIndex)
-  }
-
-  minutes = parseInt(minutes)
-  hours = parseInt(hours)
-
-  if (minutes >= 60) {
-    let q = Math.floor(minutes/60)
-    minutes = minutes % 60
-    hours += q
-  }
-
-  if (minutes == 0) {
-    if (hours == 1) {
-      output = hours + ' hour.'
+    if (hIndex < 0) {
+        hours = '0'
+        minutes = time.slice(0, mIndex)
+    } else if (mIndex < 0) {
+        hours = time.slice(0, hIndex)
+        minutes = '0'
     } else {
-      output = hours + ' hours.'
+        hours = time.slice(0, hIndex)
+        minutes = time.slice(hIndex + 1, mIndex)
     }
-  } else if (hours == 0) {
-    if (minutes == 1) {
-      output = minutes + ' minute.'
-    } else {
-      output = minutes + ' minutes.'
-    }
-  } else {
-    if (hours == 1) {
-      output = hours + ' hour and '
-    } else {
-      output = hours + ' hours and '
-    }
-    if (minutes == 1) {
-      output += minutes + ' minute.'
-    } else {
-      output += minutes + ' minutes.'
-    }
-  }
-  
-  res.render('index', {message: output});
 
-});
+    minutes = parseInt(minutes)
+    hours = parseInt(hours)
 
-module.exports = router;
+    if (minutes >= 60) {
+        let q = Math.floor(minutes / 60)
+        minutes = minutes % 60
+        hours += q
+    }
+
+    if (minutes == 0) {
+        if (hours == 1) {
+            output = hours + ' hour.'
+        } else {
+            output = hours + ' hours.'
+        }
+    } else if (hours == 0) {
+        if (minutes == 1) {
+            output = minutes + ' minute.'
+        } else {
+            output = minutes + ' minutes.'
+        }
+    } else {
+        if (hours == 1) {
+            output = hours + ' hour and '
+        } else {
+            output = hours + ' hours and '
+        }
+        if (minutes == 1) {
+            output += minutes + ' minute.'
+        } else {
+            output += minutes + ' minutes.'
+        }
+    }
+
+    res.render('index', { message: 'Timer set for ' + output })
+})
+
+module.exports = router
