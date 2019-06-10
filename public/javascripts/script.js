77e4aa5ec6873841c0e9ab9f06708afcb953c275
Daniel Bedggood <ddbedggood@gmail.com>
2019-06-10 21:22:07 +1200

    Limit timer to 100 hours

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -76,12 +76,17 @@ function timerSetMessage() {
 function remainingTime() {
     let time = new Date()
     time = new Date(time.getTime() + (s + (m + h * 60) * 60) * 1000)
+    if (time - new Date().getTime() > 1000 * 60 * 60 * 100) {
+        document.getElementById('setMessage').innerHTML = 'Timer cannot be set for longer than 100 hours.'
+        document.title = '99:59:59'
+        return
+    }
 
     const timer = setInterval(function() {
         const now = new Date().getTime()
         const remainder = time - now
 
-        const hours = Math.floor((remainder % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+        const hours = Math.floor((remainder % (1000 * 60 * 60 * 24 * 7)) / (1000 * 60 * 60))
         const minutes = Math.floor((remainder % (1000 * 60 * 60)) / (1000 * 60))
         const seconds = Math.floor((remainder % (1000 * 60)) / 1000)
 
